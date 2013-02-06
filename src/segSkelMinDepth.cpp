a80435f5e22a31f8a99186c83490a3ae50ed6596
hlocteau <herve.locteau@gmail.com>
2013-02-06 21:12:05 +0100

    in process

--- src/segSkelMinDepth.cpp
+++ src/segSkelMinDepth.cpp
@@ -7,6 +7,8 @@
 #include <io/Pgm3dFactory.h>
 #include <GrayLevelHistogram.h>
 #include <connexcomponentextractor.h>
+#include <ConnexComponentRebuilder.hpp>
+#include <geom2d.h>
 
 #include <boost/program_options/options_description.hpp>
 #include <boost/program_options/parsers.hpp>
@@ -133,6 +135,152 @@ template <typename T > void apply_translation( BillonTpl< T > &label, const QMap
 	}
 }
 
+typedef struct _TRefLine {
+	int x,z ;
+	int dx,dz ;
+	double len ;
+} TRefLine ;
+
+void set_projections( const TRefLine & refLine, QMap< arma::u16, Interval<double> > *bounds, uint nSamples, const BillonTpl< arma::u16 > &img, arma::u16 th ) {
+	BillonTpl< arma::u16 >::const_iterator iterImg = img.begin(),
+	                                       iterEndImg = img.end() ;
+	uint n_rows = img.n_rows ,
+	     n_cols = img.n_cols;
+	uint x=0,y=0,z=0;
+	
+//std::cerr<<"[ Debug ] : "<<__FUNCTION__<<" @ line "<<__LINE__<<std::endl;
+	
+	for ( ; iterImg != iterEndImg ; iterImg++ ) {
+		if ( *iterImg && *iterImg < th ) {
+			double d = ( refLine.dx * ( x - refLine.x ) + refLine.dz * ( z - refLine.z ) ) / refLine.len ;
+			if ( ! bounds[ y*nSamples/n_rows ].contains( *iterImg ) ) bounds[ y*nSamples/n_rows ].insert( *iterImg, Interval<double>( d, d ) ) ;
+			else {
+				Interval<double>& aBound = bounds[ y*nSamples/n_rows ].find( *iterImg ).value() ;
+				if ( aBound.min() > d ) aBound.setMin( d ) ;
+				else if ( aBound.max() < d ) aBound.setMax( d ) ;
+			}
+		}
+		y++ ;
+		if ( y == n_rows ) {
+			y = 0 ;
+			x++ ;
+		}
+		if ( x == n_cols ) {
+			x = 0 ;
+			z++ ;
+		}
+	}
+//std::cerr<<"[ Debug ] : "<<__FUNCTION__<<" @ line "<<__LINE__<<std::endl;
+}
+
+std::pair< uint, double > eval_projections( const QMap< arma::u16, Interval<double> > &bounds, QList< arma::u16 > *pLstBreaks ) {
+	///
+	/// \brief build a sorted list of projection intervals wrt first the min, next the max
+	/// \note to keep the linked with the corresponding identifier, we define a list of identifiers...
+	///
+	QList< arma::u16 > lexiLabels ;
+	QList< arma::u16 >::Iterator position,end_of_list ;
+	QMap< arma::u16, Interval<double> >::ConstIterator iterBound = bounds.begin(),
+	                                                   iterEndBound = bounds.end() ;
+//std::cerr<<"[ Debug ] : "<<__FUNCTION__<<" @ line "<<__LINE__<<std::endl;
+	for ( ; iterBound != iterEndBound ; iterBound++ ) {
+		position = lexiLabels.begin() ;
+		end_of_list = lexiLabels.end() ;
+		while ( position != end_of_list ) {
+			if ( bounds[ *position ].min() > iterBound.value().min() ) break ;
+			if ( bounds[ *position ].min() == iterBound.value().min() && bounds[ *position ].max() < iterBound.value().max() ) break ;
+			position++ ;
+		}
+		lexiLabels.insert( position, iterBound.key() ) ;
+	}
+	
+	position = lexiLabels.begin() ;
+	end_of_list = lexiLabels.end() ;
+	double last_max = -1 ;
+	double gap_strength = 0 ;
+	uint number_of_cluster = 0 ;
+	
+	for ( ; position != end_of_list ; position++ ) {
+		if ( bounds[ *position ].min() > last_max ) {
+			if ( last_max > 0 ) gap_strength += bounds[ *position ].min() - last_max ;
+			number_of_cluster++ ;
+			if ( pLstBreaks )
+				pLstBreaks->append( *position ) ;
+		}
+		if ( bounds[ *position ].max() > last_max ) {
+			last_max = bounds[ *position ].max() ;
+		}
+	}
+//std::cerr<<"[ Debug ] : "<<__FUNCTION__<<" @ line "<<__LINE__<<std::endl;
+	return std::pair< uint, double > ( number_of_cluster, gap_strength ) ;
+}
+
+void discriminate_projection( const BillonTpl< arma::u16 > &label, const QMap< arma::u16, Interval<double> > *bounds, uint nSamples ) {
+	BillonTpl< arma::u16 >::const_iterator iterImg = label.begin(),
+	                                       iterEndImg = label.end() ;
+	uint n_rows = label.n_rows ,
+	     n_cols = label.n_cols,
+	     n_slices = label.n_slices ;
+	uint x=0,y=0,z=0;
+
+	BillonTpl< arma::u16 > gt( n_rows, n_cols, n_slices ) ;
+	gt.fill( 0 ) ;
+	BillonTpl< arma::u16 >::iterator iterWrite = gt.begin() ;
+	
+	QMap< arma::u16, Interval<double> >::ConstIterator bound_label ;
+	QList< arma::u16 > *breaks_per_sample = new QList< arma::u16 > [ nSamples ];
+	for ( uint iSample = 0 ; iSample < nSamples; iSample++ ) {
+		uint qNum ;
+		double qCluster ;
+		boost::tie( qNum, qCluster ) = eval_projections( bounds[ iSample ], breaks_per_sample + iSample ) ;
+		std::cout<<"Sample "<<(int)iSample<<"/"<<nSamples<<"    : "<<qNum<<" cluster(s) with energy "<<qCluster<<std::endl;
+		for ( uint iCut=0;iCut < breaks_per_sample[ iSample ].size() ; iCut++ )
+			std::cout<<"\t"<<breaks_per_sample[ iSample ].at(iCut)
+			         <<" interval "<<bounds[ iSample ][ breaks_per_sample[ iSample ].at(iCut) ].min()<<" : "
+			                       <<bounds[ iSample ][ breaks_per_sample[ iSample ].at(iCut) ].max()<<std::endl;
+	}
+	
+	QList< arma::u16 > & LstBreaks = breaks_per_sample[ 0 ] ;
+	const QMap< arma::u16, Interval<double> > * current_bound = bounds + 0 ;	
+	QMap< arma::u16, Interval<double> >::ConstIterator undefined_label = current_bound->end() ;
+	arma::u16 number_of_previous_breaks = 0 ;
+	
+		
+	for ( ; iterImg != iterEndImg ; iterImg++, iterWrite++ ) {
+		if ( *iterImg ) {
+			bound_label = current_bound->constFind( *iterImg ) ;
+			if ( bound_label != undefined_label ) {
+				arma::u16 iBreak = 0 ;
+				while ( iBreak < LstBreaks.size() ) {
+					if ( bound_label.value().min() <= (*current_bound)[ LstBreaks.at( iBreak ) ].min() ) break ;
+					iBreak++ ;
+				}
+				*iterWrite = iBreak + number_of_previous_breaks;
+			}
+		}
+		y++ ;
+		if ( y == n_rows ) {
+			y = 0 ;
+			x++ ;
+		}
+		if ( y % (n_rows / nSamples) == 0 ) {
+			current_bound = bounds + ( y * nSamples/n_rows ) ;
+			undefined_label = current_bound->end() ;
+			if( y > 0 )
+				number_of_previous_breaks += breaks_per_sample[ (y-1) * nSamples/n_rows ].size() ;
+			else
+				number_of_previous_breaks = 0 ;
+			LstBreaks = breaks_per_sample[ y * nSamples/n_rows ] ;
+		}
+		if ( x == n_cols ) {
+			x = 0 ;
+			z++ ;
+		}	
+	}
+	delete [] breaks_per_sample ;
+	IOPgm3d< arma::u16, qint16, false >::write( gt, "/tmp/gt.pgm3d");
+}
+
 int main( int narg, char **argv ) {
 	TProgramArg params ;
 	if ( !process_arg( narg, argv, params ) ) return -1 ;
@@ -212,12 +360,6 @@ int main( int narg, char **argv ) {
 	SG_u8 *sg_above = new SG_u8( *classifiedVoxel, color_above ) ;
 	arma::u16 from = 1 ;
 	from = do_labeling( sg_above, *label, from ) ;
-	
-	{
-		/// \warning with command line : segSkelMinDepth --skel ~/outputData/MeMo0013/serie_2/anthill.innerskel.mk3.pgm3d --depth ~/outputData/MeMo0013/serie_2/anthillcontent.dist.pgm3d --output /tmp/segdepth.pgm3d --threshold 24
-		/// component 5 and 553 are touching!
-	
-	}
 	delete sg_above ;
 	
 	IOPgm3d< arma::u16, qint16, false >::write( *label, QString( "/tmp/rooms.pgm3d" ) ) ;
@@ -225,7 +367,7 @@ int main( int narg, char **argv ) {
 	///
 	/// Extracting corridors
 	///
-	std::cout<<"extracting corridors..."<<std::endl;
+	std::cout<<(int)from<<std::endl<<"extracting corridors..."<<std::endl;
 	SG_u8 *sg_below = new SG_u8( *classifiedVoxel, color_below ) ;
 	arma::u16 to = do_labeling( sg_below, *label, from ) ;
 	delete sg_below ;
@@ -234,10 +376,13 @@ int main( int narg, char **argv ) {
 	///
 	/// Extracting candidate merge
 	///
-	std::cout<<"extracting adjacency relations..."<<std::endl;
+	std::cout<<(int)to<<std::endl<<"extracting adjacency relations..."<<std::endl;
 	QMap< arma::u16, QList< arma::u16 > > touching ;
 	extract_adjacency( *label, touching, from ) ;
 	
+	///
+	/// a corridor being only linked to a single room can be merged with that room
+	///
 	std::cout<<"identifying merge operation..."<<std::endl;
 	QMap< arma::u16, QList< arma::u16 > >::Iterator iterRewrite = touching.begin() ;
 	QMap< arma::u16, arma::u16 > translation ;
@@ -250,6 +395,130 @@ int main( int narg, char **argv ) {
 	}
 	std::cout<<"applying "<< translation.size()<<" selected operations..."<<std::endl;
 	apply_translation( *label, translation ) ;
+	translation.clear() ;
+	
+	
+	///
+	/// reconstruct each individual connected component to evaluate their volume
+	///
+	#if 0
+	typedef ConnexComponentRebuilder< arma::u16, arma::u32, arma::u16 > TConnexComponentRebuilder ;
+	TConnexComponentRebuilder *ccr = new TConnexComponentRebuilder( *label ) ;
+	ccr->setDepth( QString( params._depthFilePath.c_str() ) ) ;
+	ccr->run() ;
+	BillonTpl< arma::u16 > *reconstruction = new BillonTpl< arma::u16 >( ccr->result() ) ;
+	delete ccr ;
+	GrayLevelHistogram<arma::u16> volumes_ext( *reconstruction ) ;
+	GrayLevelHistogram<arma::u16>::THistogram &volumes = volumes_ext._bin ;
+	/// build a sorted list of ccx (per type) wrt their volume
+	QList< arma::u16 > corridors, rooms ;
+	QList< arma::u16 > *ptr ;
+	QList< arma::u16 >::Iterator position ;
+	for ( GrayLevelHistogram<arma::u16>::THistogram::iterator it = volumes.begin() ; it != volumes.end() ; it++ ) {
+		ptr = & corridors ;
+		if ( it->first < from ) ptr = & rooms ;
+		position = ptr->begin() ;
+		while ( position != ptr->end() ) {
+			if ( volumes[ *position ] > it->second ) break ;
+			position++ ;
+		}
+		ptr->insert( position, it->first ) ;
+	}
+	#endif
+	/// criteria based on size...	
+	
+	
+	{
+		///
+		/// for algorithm design only on MeMo0013/serie_2 ...
+		///
+		uint n_cols = label->n_cols,
+		     n_rows = label->n_rows,
+		     n_slices = label->n_slices ;
+		
+		uint nSamples ;
+		std::cout<<"Number of samples to discretize the y values : " ;
+		std::cin >> nSamples ;
+		
+		QMap< arma::u16, Interval<double> > *bounds = new QMap< arma::u16, Interval<double> >[ nSamples ] ;
+		
+		TRefLine refLine ;
+		refLine.x = 0 ;
+		refLine.z = 60 ;
+		refLine.dx = 287 ;
+		
+		refLine.dz = 433 ;
+		int best_dz = -1 ;
+		uint best_num = 0, nCluster ;
+		double qCluster ;
+		for ( refLine.dz = refLine.dx ; refLine.dz < 2 * refLine.dx ; refLine.dz += 10 ) {
+			refLine.len = sqrt( refLine.dx*refLine.dx+refLine.dz*refLine.dz ) ;
+			set_projections( refLine, bounds, nSamples, *label, from ) ;
+			boost::tie( nCluster, qCluster ) = eval_projections( bounds[ nSamples/2 ],0 ) ;
+			if ( nCluster > best_num ) {
+				best_dz = refLine.dz ;
+				best_num= nCluster ;
+			}
+			for ( uint iSample = 0 ; iSample < nSamples ; iSample ++ ) bounds[ iSample ].clear() ;
+		}
+		refLine.dz = best_dz ;
+		set_projections( refLine, bounds, nSamples, *label, from ) ;
+		std::cout<<"Optimal projection on line "<<refLine.x<<","<<refLine.z<<" - "<<refLine.dx<<","<<refLine.dz<<std::endl;
+		discriminate_projection( *label, bounds, nSamples ) ;
+		BillonTpl< arma::u8 > proj_xz( n_cols, n_slices,nSamples ) ;
+		BillonTpl< arma::u8 > proj_yz( n_rows, n_slices,nSamples ) ;
+		proj_xz.fill( 0 ) ;
+		proj_yz.fill( 0 ) ;
+		proj_xz.setMaxValue(1) ;
+		proj_yz.setMaxValue(1) ;
+		BillonTpl< arma::u16 >::const_iterator iterRoom = label->begin(),
+		                                       iterRoomEnd = label->end() ;
+		uint x=0,y=0,z=0 ;
+		for ( ; iterRoom != iterRoomEnd ; iterRoom++ ) {
+			if ( *iterRoom && *iterRoom < from ) {
+				if ( true /*y*4 > 0*n_rows && y*4 < 1*n_rows*/ )
+					proj_xz(x,z,y*nSamples/n_rows ) = y*nSamples/n_rows+1 ;
+				if ( true /*x*4 > 0*n_cols && x*4 < 1*n_cols*/ )
+					proj_yz(y,z,y*nSamples/n_rows ) = y*nSamples/n_rows+1 ;
+			}
+			y++ ;
+			if ( y == n_rows ) {
+				y = 0 ;
+				x++ ;
+			}
+			if ( x == n_cols ) {
+				x = 0 ;
+				z++ ;
+			}
+		}
+		
+		for ( uint iSample = 0 ; false && iSample < nSamples ; iSample++ ) {
+			std::cout<<"= begin =======;"<<iSample+1<<std::endl;
+			for ( QMap< arma::u16,Interval<double> >::ConstIterator it = bounds[iSample].begin() ; it != bounds[iSample].end() ; it++ )
+				std::cout<<";"<<(int)it.key()<<" ;"<<it.value().min()<<" "<<it.value().max()<<";"<<std::endl;
+			std::cout<<"======== end ==;"<<std::endl;
+		}
+		delete [] bounds ;
+		
+		for ( uint iCut = 0 ; false && iCut < 20 ; iCut++ ) {
+			unsigned int len ;
+			Z2i::Point *pts = Geom2D::computeD8Segment( Z2i::Point(0,(n_slices-1)/40*(iCut+18.5)), Z2i::Point(n_cols-1,(n_slices-1)/40*(iCut+0.5)), len  ) ;
+			for ( Z2i::Point *pt = pts ; pt != pts + len ; pt++ )
+				proj_xz( pt->at(0), pt->at(1), 0 ) = 64 ;
+			delete [] pts ;
+		}
+		uint len,c ;
+		Z2i::Point *pts = Geom2D::computeD8Segment( Z2i::Point(refLine.x,refLine.z), Z2i::Point(refLine.x+refLine.dx, refLine.z+refLine.dz ) , len  ) ;
+		for ( Z2i::Point *pt = pts ; pt != pts + len ; pt++ ) {
+			if ( pt->at(0) < n_cols && pt->at(1) < n_slices )
+				for ( c = 0 ; c < proj_xz.n_slices ; c++ )
+					proj_xz( pt->at(0), pt->at(1), c ) = 96 ;
+		}
+		delete [] pts ;
+		
+		IOPgm3d< arma::u8, qint8, false >::write ( proj_xz, "/tmp/proj_xz.pgm" ) ;
+		IOPgm3d< arma::u8, qint8, false >::write ( proj_yz, "/tmp/proj_yz.pgm" ) ;
+	}
 	
 	{
 		///
@@ -262,7 +531,8 @@ int main( int narg, char **argv ) {
 	}
 	IOPgm3d< arma::u16, qint16, false >::write( *label, QString( params._outputFilePath.c_str() ) ) ;
 	delete label ;
-	/// criteria based on size...
-	
+	#if 0
+	delete reconstruction ;
+	#endif
 	return 0 ;
 }
