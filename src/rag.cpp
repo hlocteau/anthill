de70bfd4093ec7ada575aff6b67c9a9ae1ad23a9
hlocteau <herve.locteau@gmail.com>
2013-02-24 18:55:24 +0100

    fix error on EdgeData::operator +=

--- src/rag.cpp
+++ src/rag.cpp
@@ -38,13 +38,13 @@ EdgeData::EdgeData( ) {
 	_per_side[ 0 ] = boost::shared_ptr< QList<DGtal::Z3i::Point>  >(null_ptr) ;
 	_per_side[ 1 ] = boost::shared_ptr< QList<DGtal::Z3i::Point>  >(null_ptr) ;
 	#endif
-	std::cout<<"Default constructor Edge"<<std::endl;
+	//std::cout<<"Default constructor Edge"<<std::endl;
 }
 EdgeData::~EdgeData() {
-	std::cout<<"before reset "<<_source_id<<" "<<_target_id<<"  adr "<<_per_side[0]<<std::endl;
+	//std::cout<<"before reset "<<_source_id<<" "<<_target_id<<"  adr "<<_per_side[0]<<std::endl;
 	reset() ;
-	std::cout<<"Destructor Edge ";
-	std::cout<<std::endl;
+	//std::cout<<"Destructor Edge ";
+	//std::cout<<std::endl;
 }
 EdgeData::EdgeData( const EdgeData & o ) {
 	_source_id = o._source_id;
@@ -52,8 +52,8 @@ EdgeData::EdgeData( const EdgeData & o ) {
 	#ifdef STORAGE_VOXEL
 	_per_side[ 0 ] = o._per_side[ 0 ] ;
 	_per_side[ 1 ] = o._per_side[ 1 ] ;
-	std::cout<<"Copy constructor Edge ("<<_source_id<<","<<_target_id <<")";
-	std::cout<<std::endl;
+	//std::cout<<"Copy constructor Edge ("<<_source_id<<","<<_target_id <<")";
+	//std::cout<<std::endl;
 	#endif
 }
 
@@ -65,7 +65,7 @@ EdgeData & EdgeData::operator = ( const EdgeData &o ) {
 		#ifdef STORAGE_VOXEL
 		_per_side[ 0 ] = o._per_side[ 0 ] ;
 		_per_side[ 1 ] = o._per_side[ 1 ] ;
-		std::cout<<"copy operator Edge " ;
+		//std::cout<<"copy operator Edge " ;
 		std::cout<<std::endl;
 		#endif
 	}
@@ -76,10 +76,10 @@ void EdgeData::reset( ) {
 	_source_id = -1 ;
 	_target_id = -1 ;
 	#ifdef STORAGE_VOXEL
-	std::cout<<"line "<<__LINE__<<std::endl;
+	//std::cout<<"line "<<__LINE__<<std::endl;
 	_per_side[0].reset() ;
 	_per_side[1].reset() ;
-	std::cout<<"line "<<__LINE__<<std::endl;
+	//std::cout<<"line "<<__LINE__<<std::endl;
 	#endif
 }
 #ifdef STORAGE_VOXEL
@@ -100,27 +100,33 @@ void EdgeData::append( uint id, DGtal::Z3i::Point v ) {
 #endif
 
 EdgeData & EdgeData::operator += ( const EdgeData & other ) {
-	assert( _source_id == other._source_id );
-	assert( _target_id == other._target_id );
+	uint tr[] = { 0, 1 } ;
+	if ( _source_id == other._source_id && _target_id == other._target_id ) {
+	} else if ( _source_id == other._target_id && _target_id == other._source_id ) {
+		tr[0] = 1 ;
+		tr[1] = 0 ;
+	} else {
+		assert( ( _source_id == other._source_id && _target_id == other._target_id ) ||
+		        ( _source_id == other._target_id && _target_id == other._source_id ) ) ;
+	}
 	
 	for ( uint s = 0 ; s < 2 ; s++ ) {
 		uint pos_this = 0,
 		     pos_other = 0,
-		     size_this = _per_side[ s ]->size(),
-		     size_other = other._per_side[ s ]->size() ;
-		while ( pos_other != size_other && pos_this != size_this ) {
-			if ( _per_side[ s ]->at( pos_this ) == other._per_side[ s ]->at( pos_other ) ) {
+		     size_other = other._per_side[ tr[s] ]->size() ;
+		while ( pos_other != size_other && pos_this != _per_side[ s ]->size() ) {
+			if ( _per_side[ s ]->at( pos_this ) == other._per_side[ tr[s] ]->at( pos_other ) ) {
 				pos_this++ ;
 				pos_other++ ;
-			} else if ( _per_side[ s ]->at( pos_this ) > other._per_side[ s ]->at( pos_other ) ) {
-				_per_side[ s ]->insert( pos_this, other._per_side[ s ]->at( pos_other ) ) ;
-				pos_this++ ;
+			} else if ( _per_side[ s ]->at( pos_this ) > other._per_side[ tr[s] ]->at( pos_other ) ) {
+				_per_side[ s ]->insert( pos_this, other._per_side[ tr[s] ]->at( pos_other ) ) ;
+				pos_other++ ;
 			} else {
 				pos_this++ ;
 			}
 		}
 		while ( pos_other != size_other ) {
-			_per_side[ s ]->append( other._per_side[ s ]->at( pos_other ) ) ;
+			_per_side[ s ]->append( other._per_side[ tr[s] ]->at( pos_other ) ) ;
 			pos_other++ ;
 		}
 	}
@@ -141,13 +147,13 @@ void merge_nodes( GraphAdj::vertex_descriptor growing, GraphAdj::vertex_descript
 	bool existing,creation ;
 	boost::tie( in_edge, in_edge_end ) = boost::in_edges(other, g) ;
 	for ( ; in_edge != in_edge_end ; in_edge++ ) {
-		std::cout<<"info edge "<<boost::source( *in_edge, g )<<" "<<boost::target( *in_edge, g )<<" in loop"<<std::endl;
+		std::cout<<"\tinfo edge "<<boost::source( *in_edge, g )<<" "<<boost::target( *in_edge, g )<<" in loop"<<std::endl;
 		assert( boost::target( *in_edge, g ) == other ) ;
 		opposite = boost::source( *in_edge, g ) ;
 		EdgeData & ed = edge_map[ *in_edge ] ;
 		
 		if ( opposite == growing ) {
-			std::cout<<"info edge link specific pair of vertices"<<std::endl;
+			std::cout<<"\t\tinfo edge link specific pair of vertices"<<std::endl;
 			ed.reset() ;
 			continue ;
 		}
@@ -157,30 +163,25 @@ void merge_nodes( GraphAdj::vertex_descriptor growing, GraphAdj::vertex_descript
 		
 		boost::tie( e, existing ) = boost::edge( opposite, growing, g ) ;
 		if ( !existing ) {
-			std::cout<<"info edge has to be created"<<std::endl;
+			std::cout<<"\t\tinfo edge has to be created"<<std::endl;
 			boost::tie( e, creation ) = boost::add_edge( opposite, growing, g ) ;
 			assert( boost::source(e,g) == opposite ) ;
 			assert( boost::target(e,g) == growing ) ;
-			
-			ed.setTarget( growing ) ;
+			if ( ed.target_id() == other )
+				ed.setTarget( growing ) ;
+			else
+				ed.setSource( growing ) ;
 			edge_map[ e ] = ed ;
 		} else {
-			std::cout<<"info edge has to be updated"<<std::endl;
+			std::cout<<"\t\tinfo edge has to be updated"<<std::endl;
 			assert( boost::source(e,g) == opposite ) ;
 			assert( boost::target(e,g) == growing ) ;
-			std::cerr<<"info : ed "<<ed.source_id()<<" - "<<ed.target_id()<<std::endl;
+			std::cerr<<"\t\t\tinfo : ed "<<ed.source_id()<<" - "<<ed.target_id()<<std::endl;
 			EdgeData & edg = edge_map[ e ] ;
-			std::cerr<<"info : edg "<<edg.source_id()<<" - "<<edg.target_id()<<std::endl;
-			if ( edg.source_id() == boost::target(e,g) && boost::source(e,g) == edg.target_id() ) {
-				edg.setSource( boost::source(e,g) ) ;
-				edg.setTarget( boost::target(e,g) ) ;
-				std::cerr<<"info : reverse edge"<<std::endl;
-			}
-			std::cerr<<"info : edg "<<edg.source_id()<<" - "<<edg.target_id()<<std::endl;
-			ed.setTarget( growing ) ;
-			std::cerr<<"info : edg "<<edg.source_id()<<" - "<<edg.target_id()<<std::endl;
-			assert( boost::source(e,g) == edg.source_id() &&
-			        boost::target(e,g) == edg.target_id() ) ;
+			std::cerr<<"\t\t\tinfo : edg "<<edg.source_id()<<" - "<<edg.target_id()<<std::endl;
+			if ( ed.target_id() == other ) ed.setTarget( growing ) ;
+			if ( ed.source_id() == other ) ed.setSource( growing ) ;
+			std::cerr<<"\t\t\tinfo : ed "<<ed.source_id()<<" - "<<ed.target_id()<<std::endl;
 			edg += ed ;
 		}
 		ed.reset() ;
