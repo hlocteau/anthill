1d37239a631004df1b5bcbb791b6a514b45d14ea
hlocteau <herve.locteau@gmail.com>
2013-02-24 17:11:24 +0100

    define here filtering image

--- include/utils.h
+++ include/utils.h
@@ -4,4 +4,25 @@
 #include <GrayLevelHistogram.h>
 
 arma::Mat<arma::u8> * dilate( const arma::Mat<arma::u8> &im, int row_radius, int col_radius ) ;
+
+
+template<typename T> BillonTpl<arma::u8> * filter_low( const BillonTpl<T> &data, T th ) {
+	BillonTpl< arma::u8 > *plow = new BillonTpl< arma::u8 > ( data.n_rows, data.n_cols, data.n_slices ) ;
+	BillonTpl< arma::u8 >::iterator iterLow = plow->begin(),
+	                                iterLowEnd = plow->end() ;
+	typename BillonTpl< T >::const_iterator iterData = data.begin() ;
+	for ( ; iterLow != iterLowEnd ; iterLow++, iterData++ )
+		*iterLow = ( *iterData > 0 && *iterData < th ? 1 : 0 ) ;
+	return plow ;
+}
+template<typename T> BillonTpl<arma::u8> * filter_high( const BillonTpl<T> &data, T th ) {
+	BillonTpl< arma::u8 > *phigh = new BillonTpl< arma::u8 > ( data.n_rows, data.n_cols, data.n_slices ) ;
+	BillonTpl< arma::u8 >::iterator iterHigh = phigh->begin(),
+	                                iterHighEnd = phigh->end() ;
+	typename BillonTpl< T >::const_iterator iterData = data.begin() ;
+	for ( ; iterHigh != iterHighEnd ; iterHigh++, iterData++ )
+		*iterHigh = ( *iterData > th ? 1 : 0 ) ;
+	return phigh ;
+}
+
 #endif
